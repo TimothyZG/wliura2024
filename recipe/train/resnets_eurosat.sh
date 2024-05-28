9ed979c145884e884b433ec55701ef650fe9f64f
TimothyZG <tzhou13@student.ubc.ca>
2024-05-28 11:42:54 -0700

    asked for more workers and gpu time for eurosat

--- recipe/train/resnets_eurosat.sh
+++ recipe/train/resnets_eurosat.sh
@@ -3,13 +3,13 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --ntasks-per-node=4
-#SBATCH --time=04:00:00
+#SBATCH --time=08:00:00
 #SBATCH --mail-user=<tiange.zhou@outlook.com>
 #SBATCH --mail-type=ALL
 #SBATCH --output=recipe/train/output/slurm-%j.out
 module purge
 module load python/3.10 scipy-stack
 source ~/py310/bin/activate
-python recipe/train/train_resnet.py -d EuroSAT -m Resnet18 -pn EuroSAT -o ADAM
-python recipe/train/train_resnet.py -d EuroSAT -m Resnet50 -pn EuroSAT -o ADAM
-python recipe/train/train_resnet.py -d EuroSAT -m Resnet101 -pn EuroSAT -o ADAM
+python recipe/train/train_resnet.py -d EuroSAT -m Resnet18 -pn EuroSAT -o ADAM -n 4
+python recipe/train/train_resnet.py -d EuroSAT -m Resnet50 -pn EuroSAT -o ADAM -n 4
+python recipe/train/train_resnet.py -d EuroSAT -m Resnet101 -pn EuroSAT -o ADAM -n 4
