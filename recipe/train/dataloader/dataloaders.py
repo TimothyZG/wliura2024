67b72478276672dd6422016931e584af2f1c889b
TimothyZG <tzhou13@student.ubc.ca>
2024-05-28 11:43:21 -0700

    added iwildcam to resnet recipe

--- recipe/train/dataloader/dataloaders.py
+++ recipe/train/dataloader/dataloaders.py
@@ -5,6 +5,9 @@ import os
 from torchvision import transforms
 from torch.utils.data import DataLoader, Subset
 from sklearn.model_selection import train_test_split
+from wilds import get_dataset
+from wilds.common.data_loaders import get_train_loader, get_eval_loader
+
 random_state = 42 # For reproducibility
 
 transform_routine = transforms.Compose([
@@ -37,6 +40,13 @@ def get_dataloader(ds,root,bs,nworkers):
         train_loader = DataLoader(train, batch_size=bs, shuffle=True, num_workers=nworkers)
         val_loader = DataLoader(val, batch_size=bs, shuffle=False, num_workers=nworkers)
         test_loader = DataLoader(test, batch_size=bs, shuffle=False, num_workers=nworkers)
+    elif(ds=="iWildCam"):
+        dataset = get_dataset(dataset="iwildcam", root_dir=root, download=True)
+        train_data = dataset.get_subset("train", transform=transform_routine)
+        train_loader = get_train_loader("standard", train_data, batch_size=bs, num_workers=nworkers)
+        test_data = dataset.get_subset("test", transform=transform_routine)
+        test_loader = get_eval_loader("standard", test_data, batch_size=bs, num_workers=nworkers)
+        val_loader = None
     else:
         raise Exception(f"Unrecognized dataset provided to get_dataloader: {ds}, check spelling or implement get_numclass if working with new dataset")
     return train_loader,val_loader,test_loader
@@ -46,5 +56,7 @@ def get_numclass(ds):
         return 47
     elif(ds=="EuroSAT"):
         return 10
+    elif(ds=="iWildCam"):
+        return 182
     else:
         raise Exception(f"Unrecognized dataset provided to get_numclass :{ds}, check spelling or implement get_numclass if working with new dataset")
---
+++ recipe/train/resnets_iwildcam.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mem=32G
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=08:00:00
+#SBATCH --mail-user=<tiange.zhou@outlook.com>
+#SBATCH --mail-type=ALL
+#SBATCH --output=recipe/train/output/slurm-%j.out
+module purge
+module load python/3.10 scipy-stack
+source ~/py310/bin/activate
+python recipe/train/train_resnet.py -d iWildCam -m Resnet18 -pn iWildCam -o ADAM -n 4 -e 40
+python recipe/train/train_resnet.py -d iWildCam -m Resnet50 -pn iWildCam -o ADAM -n 4 -e 40
+python recipe/train/train_resnet.py -d iWildCam -m Resnet101 -pn iWildCam -o ADAM -n 4 -e 40
