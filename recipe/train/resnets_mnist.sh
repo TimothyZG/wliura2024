582c6f33966cb6d508a75b37b1dee8cad208805b
TimothyZG <tzhou13@student.ubc.ca>
2024-06-03 13:04:21 -0700

    added more shell code for different datasets

---
+++ recipe/train/resnets_mnist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mem=32000M
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=12:00:00
+#SBATCH --mail-user=<tiange.zhou@outlook.com>
+#SBATCH --mail-type=ALL
+#SBATCH --output=recipe/train/output/slurm-%j.out
+module purge
+module load python/3.10 scipy-stack
+source ~/py310/bin/activate
+python recipe/train/train_resnet.py -d MNIST -m Resnet18 -pn MNIST -o ADAM -n 4 -e 30 -g 0.95 -r $SLURM_TMPDIR/data
+python recipe/train/train_resnet.py -d MNIST -m Resnet50 -pn MNIST -o ADAM -n 4 -e 30 -g 0.95 -r $SLURM_TMPDIR/data
+python recipe/train/train_resnet.py -d MNIST -m Resnet101 -pn MNIST -o ADAM -n 4 -e 30 -g 0.95 -r $SLURM_TMPDIR/data
---
+++ recipe/train/resnets_svhn.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --mem=32000M
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --ntasks-per-node=4
+#SBATCH --time=12:00:00
+#SBATCH --mail-user=<tiange.zhou@outlook.com>
+#SBATCH --mail-type=ALL
+#SBATCH --output=recipe/train/output/slurm-%j.out
+module purge
+module load python/3.10 scipy-stack
+source ~/py310/bin/activate
+python recipe/train/train_resnet.py -d SVHN -m Resnet18 -pn SVHN -o ADAM -n 4 -e 30 -g 0.95 -r $SLURM_TMPDIR/data
+python recipe/train/train_resnet.py -d SVHN -m Resnet50 -pn SVHN -o ADAM -n 4 -e 30 -g 0.95 -r $SLURM_TMPDIR/data
+python recipe/train/train_resnet.py -d SVHN -m Resnet101 -pn SVHN -o ADAM -n 4 -e 30 -g 0.95 -r $SLURM_TMPDIR/data
