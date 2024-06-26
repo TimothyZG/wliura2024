03422ed5328fbf05bebaec8a014b59fa2719d6d2
TimothyZG <tzhou13@student.ubc.ca>
2024-06-25 22:44:43 -0700

    updated cifar10 shell code

--- recipe/train/resnets_cifar10.sh
+++ recipe/train/resnets_cifar10.sh
@@ -10,6 +10,6 @@
 module purge
 module load python/3.10 scipy-stack
 source ~/py310/bin/activate
-python recipe/train/train_resnet.py -d CIFAR10 -m Resnet18 -pn CIFAR10 -o ADAM -n 4 -e 40 -g 0.97 -r $SLURM_TMPDIR/data --lr 0.0015
-python recipe/train/train_resnet.py -d CIFAR10 -m Resnet50 -pn CIFAR10 -o ADAM -n 4 -e 50 -g 0.97 -r $SLURM_TMPDIR/data --lr 0.0015
-python recipe/train/train_resnet.py -d CIFAR10 -m Resnet101 -pn CIFAR10 -o ADAM -n 4 -e 60 -g 0.97 -r $SLURM_TMPDIR/data --lr 0.0015
+python recipe/train/train_resnet.py -d CIFAR10 -m Resnet18 -pn CIFAR10 -o ADAM -n 4 -e 75 -r $SLURM_TMPDIR/data -ss 15 --lr 0.005 -lrs NONE --batch_size 128
+python recipe/train/train_resnet.py -d CIFAR10 -m Resnet50 -pn CIFAR10 -o ADAM -n 4 -e 100 -r $SLURM_TMPDIR/data -ss 20 --lr 0.005 -lrs NONE --batch_size 128
+python recipe/train/train_resnet.py -d CIFAR10 -m Resnet101 -pn CIFAR10 -o ADAM -n 4 -e 125 -r $SLURM_TMPDIR/data -ss 25 --lr 0.005 -lrs NONE --batch_size 128
