8f0afe2cf5838c37ebc8ce225f876276a897b8ca
TimothyZG <tzhou13@student.ubc.ca>
2024-08-03 09:41:58 -0700

    modified tuning and training code for iwc

--- recipe/train-iwc/hyper-tune-iwc.sh
+++ recipe/train-iwc/hyper-tune-iwc.sh
@@ -4,12 +4,12 @@
 #SBATCH --gres=gpu:v100l:4
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
-#SBATCH --time=6:00:00
+#SBATCH --time=24:00:00
 #SBATCH --mail-user=<tiange.zhou@outlook.com>
 #SBATCH --mail-type=ALL
-#SBATCH --output=recipe/iwc/output/slurm-%j.out
+#SBATCH --output=recipe/train-iwc/output/slurm-%j.out
 module purge
 module load python/3.10 scipy-stack
 source ~/py310/bin/activate
 
-python recipe/iwc/hyper-param-tune.py --config-path "recipe/iwc/config.yaml"
+python recipe/train-iwc/hyper-param-tune.py --config-path "recipe/train-iwc/config.yaml"
